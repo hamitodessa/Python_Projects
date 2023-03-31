08701e9616ce15b6097b83117358a48166249a7d
hamit <hamit@okumus.com>
2023-03-31 17:27:44 +0300

    1

---
+++ Python_Denemeler/Denemeler3.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 30, 2023
+
+@author: hamit
+'''
+class CARI_ACCESS:
+    
+    _ICari = Animal
+    
+     
+
+    def __init__(self, Animal):
+        self._ICari = Animal
+        
+    def baglan(self):
+        self._ICari.speak()
+        
+        
+    
---
+++ Python_Denemeler/baglan.py
@@ -0,0 +1,29 @@
+'''
+Created on Mar 30, 2023
+
+@author: hamit
+'''
+from abc import *
+class DBInterface(ABC):
+   @abstractmethod
+   def connect(self):
+       pass
+   @abstractmethod
+   def disconnect(self):
+       pass
+class Oracle(DBInterface):
+   def connect(self):
+       print('Connecting to Oracle Database...')
+   def disconnect(self):
+       print('Disconnecting to Oracle Database...')
+class Sybase(DBInterface):
+   def connect(self):
+       print('Connecting to Sybase Database...')
+   def disconnect(self):
+       print('Disconnecting to Sybase Database...')   
+#dbname=input('Enter Database Name either Oracle or Sybase:')
+dbname="Oracle"
+classname=globals()[dbname]
+x=classname()
+x.connect()
+x.disconnect()
