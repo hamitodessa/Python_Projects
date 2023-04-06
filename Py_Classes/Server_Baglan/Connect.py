7c4519c2ea1e821b0df9fbe69e5c12dec983dd73
hamit <hamit@okumus.com>
2023-04-06 18:29:43 +0300

    1

--- Py_Classes/Server_Baglan/Connect.py
+++ Py_Classes/Server_Baglan/Connect.py
@@ -7,7 +7,6 @@ Created on Apr 6, 2023
 class Connect():
     def __init__(self, IConnection):
         self.ICn = IConnection
-        print("qaz" + IConnection.__name__)
     def Server_kontrol_L(self,inst, kull, sifre,  portt):
         return self.ICn.Server_kontrol_L(self,  inst, kull, sifre,  portt)
     def Server_kontrol_S(self,server, inst, kull,  sifre, port):
--- Py_Classes/Server_Baglan/OBS_Ortak_MsSql.py
+++ Py_Classes/Server_Baglan/OBS_Ortak_MsSql.py
@@ -5,18 +5,27 @@ Created on Apr 6, 2023
 '''
 
 from Server_Baglan.IConnection import IConnection
-import pypyodbc
+import pypyodbc 
 
 
 class Obs_Ortak_MsSql(IConnection):
     def Server_kontrol_L(self,inst, kull, sifre,  port):
         try:
-            pypyodbc.connect('DRIVER={SQL Server};SERVER=localhost;instanceName=' + inst +';')
-            return True
+            print( inst)
+            #connSqlServer = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=192.106.0.102\instance1;DATABASE=master;UID=sql2008;PWD=password123')
+            #pyodbc.connect()
+            instt = "\\" + inst
+            conn = pypyodbc.connect(r'DRIVER={SQL SERVER};Server=localhost' + instt + ';Trust_Connection =True')
+            print(conn)
+            if conn :
+                return True
+            else:
+                return False
         except:
             return False
     def Server_kontrol_S(self,server, inst, kull,  sifre, port):
         try:
+            #connSqlServer = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=192.106.0.102,1443;DATABASE=master;UID=sql2008;PWD=password123')
             pypyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';instanceName=' + inst +';UID='+ kull+';PWD=' + sifre)
             return True
         except:
